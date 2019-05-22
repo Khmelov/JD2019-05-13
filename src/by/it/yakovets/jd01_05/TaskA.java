9bc6d3ae69388d574e3fcad4e0a1e98f19a16ec2
Evgeniy Yakovets <49934014+jekson0702@users.noreply.github.com>
2019-05-22 21:48:52 +0300

    started jd01_05

---
+++ src/by/it/yakovets/jd01_05/TaskA.java
@@ -0,0 +1,24 @@
+package by.it.yakovets.jd01_05;
+import static java.lang.Math.*;
+public class TaskA {
+    public static void main(String[] args) {
+        step1();
+//        step2();
+//        step3();
+    }
+
+
+
+    private static void step1() {
+    double a=756.13;
+    double x=0.3;}}
+//    double p1=cos(pow(pow(x,2)+PI/6.5));
+////    }
+//
+//    private static void step2() {
+//    }
+//
+//    private static void step3() {
+//    }
+//}
+//
