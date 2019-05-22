7ff503e2bc89152ec5d5840fbae9ccf5c34af849
Dima <nevar.dima@gmail.com>
2019-05-22 21:21:52 +0300

    start jd01_05

---
+++ src/by/it/dnevar/jd01_05/TaskA.java
@@ -0,0 +1,20 @@
+package by.it.dnevar.jd01_05;
+
+public class TaskA {
+    public static void main(String[] args) {
+        step1();
+        step2();
+        step3();
+    }
+    private static void step1(){
+
+    }
+
+    private static void step2(){
+
+    }
+
+    private static void step3(){
+
+    }
+}
