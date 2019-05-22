b65bcfb578c560615cd44109b75fb8003939fcd8
iZaykoff <zaykoffzay@gmail.com>
2019-05-23 00:23:42 +0300

    start jd01_05

---
+++ src/by/it/izaykoff/jd01_01/Hello.java
@@ -0,0 +1,16 @@
+package by.it.izaykoff.jd01_01;
+
+public class Hello {
+
+    private String slogan = "Hello World!";
+
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;
+    }
+
+    void prinSlogan(){
+        System.out.println(slogan);
+    }
+
+
+}
---
+++ src/by/it/izaykoff/jd01_01/Main.java
@@ -0,0 +1,17 @@
+package by.it.izaykoff.jd01_01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Hello hello = new Hello();
+        hello.prinSlogan();
+        hello.setSlogan("Привет мир!");
+        hello.prinSlogan();
+
+        Args argObject = new Args(args);
+        argObject.printArgs();
+
+
+    }
+}
