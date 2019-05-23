3557132d5f5af1ab329b78d771ea25ce67869646
Windows7 <guchi5550@gmail.com>
2019-05-24 02:40:14 +0300

    Задание 5-а

--- src/by/it/guchek/jd01_05/TaskB.java
+++ src/by/it/guchek/jd01_05/TaskB.java
@@ -28,13 +28,15 @@ public class TaskB {
         }
     }
     private static void step1c() {
-       //double lamda, beta;
-
-        //for (double -6<x1; x1 < 2 ; x1=x1+0.5){
-          //  double xres=x1/2;
-            //if xres >
-        //}
-
+       /*
+        double lamda, beta;
+        for (double x>=-6; x < 2 ; x=x+0.5){
+          double (xres=x/2) beta= atan(x*x);
+        if (xres=0.2) beta= 1/tan(x*x);
+        if xres x>-2 & <0.2
+        if xres
+        }
+        */
 
 
     }
