53bc7bb4f194c0e567a8e4e87fa6ce19bb22aa64
Evgeniy Yakovets <49934014+jekson0702@users.noreply.github.com>
2019-05-23 22:24:51 +0300

    started jd01_05

--- src/by/it/yakovets/jd01_04/asdad.java
+++
@@ -1,9 +0,0 @@
-package by.it.yakovets.jd01_04;
-
-public class asdad {
-    public static void main(String[] args) {
-        int[]mas={4,5,8,9,6};
-        int m=mas.length/2;
-        System.out.println(m);
-    }
-}
