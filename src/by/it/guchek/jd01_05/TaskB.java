d687db5adbeed91e1119ef7b0c98c6665ed69749
Windows7 <guchi5550@gmail.com>
2019-05-24 02:15:56 +0300

    Задание 5-а

--- src/by/it/guchek/jd01_05/TaskB.java
+++ src/by/it/guchek/jd01_05/TaskB.java
@@ -8,7 +8,7 @@ import static java.lang.StrictMath.pow;
 public class TaskB {
     public static void main(String[] args){
         step1b ();
-        step1c();
+        //step2c();
 
     }
 
@@ -16,32 +16,26 @@ public class TaskB {
     private static void step1b() {
         double y = 0;
         for (double a = 0; a <=2 ; a=a+0.2) {
-            double sum1;
+            double sum1=0;
             for (int x = 1; x <=6 ; x++) {
-                sum1=(Math.pow(7,a)-cos(x));
-                y=sum1+y;
+                sum1=(Math.pow(7,a));
+                sum1=sum1+sum1;
+                //System.out.println(y);
+                y=sum1-cos(x);
             }
 
-            System.out.printf("%10.20f\n",y);
+            System.out.println(y);
         }
     }
     private static void step1c() {
+       //double lamda, beta;
 
-        double lamda, beta, xres;
+        //for (double -6<x1; x1 < 2 ; x1=x1+0.5){
+          //  double xres=x1/2;
+            //if xres >
+        //}
 
-        for (double x=-5.5; x < 2 ; x+=0.5){
-          xres=x/2;
-        if (xres/2>-2 && xres <=-1)
-            beta= sin(x*x);
-        else  if (xres>-1 && xres<0.2)
-                beta= cos(x*x);
-        else  if (xres==0.2)
-                beta=1/tan(x*x);
-        else continue;
 
-            lamda=log10(abs(beta+2.74));
-            System.out.printf("%s%-1.2f%8s%5.10f\n", "Если x/2= ", xres, "Лямда= ",lamda);
-        }
 
     }
 }
