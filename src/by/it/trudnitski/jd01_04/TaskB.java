4099929d8ca3d21ec64c4019943f113453dbd36f
RudnitskTimofei <3761544Radiohead>
2019-05-24 20:25:30 +0300

    Привет

--- src/by/it/trudnitski/jd01_04/TaskB.java
+++ src/by/it/trudnitski/jd01_04/TaskB.java
@@ -6,57 +6,39 @@ public class TaskB {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         String[]names=new String[n];
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < n; i++) {
-            names[i] = scanner.nextLine();
-            }
-
+        for (int i = 0; i <names.length; i++) {
+            names[i] = sc.next();
+        }
         int[][] salary=new int[n][4];
         for (int i = 0; i < names.length; i++) {
-            salary[0][i] = i;
-        }
-
-        for (int i = 0; i < salary.length; i++) {
-            System.out.println("Введите зарплату для"+" "+names[i]);
-
+            System.out.printf("Введите зарплату для %s\n",names[i]);
             for (int j = 0; j < salary[i].length; j++) {
                 salary[i][j]=sc.nextInt();
             }
         }
         System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%-10s",
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s%-10s\n",
                 "Фамилия", "Квартал1", "Квартал2", "Квартал3", "Квартал4", "Итого");
-        System.out.println();
         System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
         int sum=0;
         int res=0;
         int k=0;
-
         for (int i = 0; i < salary.length; i++) {
             System.out.printf("%-10s",names[i]);
             for (int j = 0; j < salary[i].length; j++) {
                 System.out.printf("%-10d",salary[i][j]);
                sum+=salary[i][j];
                               k++;
-
-
-
-
             }
             res=res+sum;
             System.out.printf("%-10d",sum);
             sum=0;
             System.out.println();
-
         }
-
         double middle = (double)res / k;
-
         System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
-
-        System.out.printf("%-10s%-10d%n%-10s%-10.4f","Итого",res,"Средняя",middle);
+        System.out.printf("%-10s%-10d%n%-10s%-10.3f","Итого",res,"Средняя",middle);
 
 
 
