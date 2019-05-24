d8f3fbe3ed1a37ad46cce451d050375f0e1029df
YanaKobrinets <y.kobrinets@gmail.com>
2019-05-24 11:57:35 +0300

    started jd05

--- src/by/it/kobrinets/jd01_05/TaskB.java
+++ src/by/it/kobrinets/jd01_05/TaskB.java
@@ -2,16 +2,21 @@ package by.it.kobrinets.jd01_05;
 
 public class TaskB {
     public static void main(String[] args) {
-step1();
+        step1();
+        step2();
     }
 
     private static void step1() {
-        for (int x = 1; x <= 6; x++) {
         for (double a = 0; a <= 2; a = a + 0.2) {
-                double y = Math.pow(7, a) - Math.cos(x);
-                System.out.printf("При a = %.2f Сумма y = %f.4\n", a, y);
+            double y = 0;
+            for (int x = 1; x <= 6; x++) {
+                y = y + (Math.pow(7.0, a)) - Math.cos(x);
+
             }
+            System.out.printf("При a = %.2f Сумма y = %g\n", a, y);
+
         }
+
     }
 
 
@@ -19,21 +24,18 @@ step1();
 
         double a, b;
 
-        for (double x = -5.5; x < 2; x = x + 0.5){
+        for (double x = -5.5; x < 2; x = x + 0.5) {
             if (x / 2 > -2 && x / 2 <= -1) {
                 b = Math.sin(Math.pow(x, 2));
-            }
-            else if (x / 2 > -1 && x / 2 < 0.2) {
+            } else if (x / 2 > -1 && x / 2 < 0.2) {
                 b = Math.cos(Math.pow(x, 2));
-            }
-            else if (x /2 == 0.2) {
+            } else if (x / 2 == 0.2) {
                 b = 1 / (Math.tan(Math.pow(x, 2)));
-            }
-            else continue;
+            } else continue;
 
             a = Math.log10(Math.abs(b + 2.74));
 
-            System.out.printf("%s%-1.2f%s=%-5.10f\n", "При x/2=", x/2, " a", a);
+            System.out.printf("%s%-1.2f%s=%-5.10f\n", "При x/2=", x / 2, " a", a);
 
         }
 
