ba23445adf60f6ec38f485f62cab8ad101549ac1
AdamvichAS <anton.adamovich1010@gmail.com>
2019-05-26 03:34:25 +0300

    06_B1

--- src/by/it/adamovichjr/jd01_06/TaskB1.java
+++ src/by/it/adamovichjr/jd01_06/TaskB1.java
@@ -13,10 +13,9 @@ public class TaskB1 {
     private static void test(String[]mas){
         String soglasn = "у е ы а о э я и ю ё У Е Ы А О Э Я И Ю Ё";
         for (int i = 0; i < mas.length; i++) {
-
             String first = String.valueOf(mas[i].charAt(0));
             String last = String.valueOf(mas[i].charAt(mas[i].length() - 1));
-            if (soglasn.contains(first) != true && soglasn.contains(last))
+            if (!soglasn.contains(first) && soglasn.contains(last))
                 System.out.println(mas[i]);
         }
     }
