4d687d8fa476e34e3dbfde2b78a40a5e3177940a
Windows7 <guchi5550@gmail.com>
2019-05-26 08:51:21 +0300

    Задание 6-B2-2

--- src/by/it/guchek/jd01_06/TaskB2.java
+++ src/by/it/guchek/jd01_06/TaskB2.java
@@ -6,37 +6,37 @@ import java.util.regex.Pattern;
 
 public class TaskB2 {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder(Poem.text);
-        Pattern pattern = Pattern.compile("[а-яА-ЯЁё[^.?!]+]+"); //pettern описывающий все слова русского языка от 4-х символов
+        //StringBuilder sb = new StringBuilder(Poem.text);
+        Pattern pattern = Pattern.compile("[а-яА-ЯЁё[^?!.]]+"); //pettern описывающий все слова русского языка от 4-х символов
         //String [] mas = pattern.split("[а-яА-ЯЁё[^.?!]]+");
-
         Matcher matcher = pattern.matcher(Poem.text);
-        int i=0;
-        int [] countchar = new int[14];
+        int i = 0;
+        int[] countchar = new int[14];
 
         while (matcher.find()) {
 
-            //String predl = matcher.group();
-            String predl = matcher.group().replaceAll("[\\s:;.,-]+"," ").trim();
+            String predl = matcher.group().replaceAll("[\\s:;.,-]+", " ").trim();
             System.out.println(predl);
-            countchar[i]=predl.length();
-            System.out.print(countchar[i]);
+            countchar[i] = predl.length();
+            //System.out.print(countchar[i]);
             i++;
         }
+
         System.out.println(i);
-        //int [] countchar = new int [i];
+
         Arrays.sort(countchar);
 
         //for (int i1 = 0; i1 < countchar.length; i1++)  System.out.println(countchar[i1]);
 
 
-
-        while (matcher.find()) {
-            int j=0;
-            String predl = matcher.group().replaceAll("[\\s:;.,-]+"," ").trim();
-            if (predl.length()==countchar[j])
-            {System.out.print(predl);}
-            j++;
+        for (int i1 = 0; i1 < countchar.length; i1++) {
+            matcher.reset();
+            while (matcher.find()) {
+                String predl = matcher.group().replaceAll("[\\s:;.,-]+", " ").trim();
+                if (predl.length() == countchar[i1]){
+                    System.out.println(predl);
+                break;}
+            }
         }
     }
 }
