83c2cffb73bb594710b02c4822508b64c9eaa2ab
Windows7 <guchi5550@gmail.com>
2019-05-28 00:24:48 +0300

    Задание 7-А

---
+++ src/by/it/guchek/jd01_07/Scalar.java
@@ -0,0 +1,9 @@
+package by.it.guchek.jd01_07;
+
+class Scalar extends Var{                                    //убрала публичный т.к. больше нигде не будет виден
+
+    @Override
+    public String toString() {
+        return "это класс Scalar";
+    }
+}
---
+++ src/by/it/guchek/jd01_07/Var.java
@@ -0,0 +1,9 @@
+package by.it.guchek.jd01_07;
+
+abstract class Var {                 //за пределами класса не используетсям убрали public
+
+    @Override
+    public String toString() {
+        return "Это абстрактный класс Var";
+    }
+}
