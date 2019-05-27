2f73ccb0b52b6f2eba0df3d752d9beb1bf063898
dmitrijorlov <MyPassword#4>
2019-05-27 14:19:50 +0300

    jd01_061c comp

--- src/by/it/orlov/jd01_06/TaskC1.java
+++ src/by/it/orlov/jd01_06/TaskC1.java
@@ -1,19 +1,48 @@
 package by.it.orlov.jd01_06;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskC1 {
     public static void main(String[] args) {
+
         String t = Poem.text;
-        String[] rows = t.split("\\n");
-        int o = 0;
-        for (int i = 0; i < rows.length; i++) {
-            String[] word = rows[i].split(" ");
-            int spaces = rows[i].length() - rows[i].replace(" ", "").length();
-            int s = 36-rows[i].length();
-            for (int i1 = 0; i1 < word.length; i1++) {
-                System.out.print(word[i]);
+        String[] str = t.split("\\n");
+        int o = str[0].length();
+        for (int i = 1; i < str.length; i++) {
+            if(str[i].length() > o) {
+                o = str[i].length();
+            }
+        }
+        System.out.println(checker(o, str));
+
+    }
+    private static StringBuilder checker(int o, String[] str){
+        Pattern target = Pattern.compile("[^\\s][ ]+[^\\s]");
+        StringBuilder chen = new StringBuilder();
+        StringBuilder sub = new StringBuilder();
+
+        for (String s : str) {
+            chen.append(s.trim());
+            Matcher match = target.matcher(chen);
+
+            while (chen.length() < o) {
+                int beg = 0;
+                while (match.find(beg)) {
+                    if (chen.length() == o) {
+                        break;
+                    }
+                    String gr = match.group();
+                    int spot = match.start();
+                    chen.insert(spot + 1, gr.charAt(1));
+                    beg = match.end();
+                }
 
             }
-
+            sub.append(chen);
+            sub.append("\n");
+            chen.delete(0, chen.length());
         }
+        return sub;
     }
 }
