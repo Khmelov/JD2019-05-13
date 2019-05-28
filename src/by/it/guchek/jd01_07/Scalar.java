78de0da819bf889089d4274aa0cd7c11d8087bd4
Windows7 <guchi5550@gmail.com>
2019-05-28 22:06:16 +0300

    Задание 7-Аcomplit

--- src/by/it/guchek/jd01_07/Scalar.java
+++ src/by/it/guchek/jd01_07/Scalar.java
@@ -1,9 +1,26 @@
 package by.it.guchek.jd01_07;
 
 class Scalar extends Var{                                    //убрала публичный т.к. больше нигде не будет виден
+    private double value;
+
+
+    Scalar(double value) {  //конструктор будет принимать какое то значение value и заносить это значение
+                                   //во внутреннее поле  // тоже убрали публичный
+        this.value = value;
+    }
+
+
+    Scalar(String strScalar){
+        this.value = Double.parseDouble(strScalar);  //преобразует строковое значение к double
+    }
+
+    Scalar (Scalar scalar){           //принимает на вход экземпляр этого класса
+
+        this.value = scalar.value; // заполним поле value в this,  а берем это значение из класса scalar
+    }
 
     @Override
     public String toString() {
-        return "это класс Scalar";
+        return Double.toString(value);   //переопределили метод String toString, чтобы он
     }
 }
---
+++ src/by/it/guchek/jd01_07/Vector.java
@@ -0,0 +1,10 @@
+package by.it.guchek.jd01_07;
+
+class Vector extends Var  {
+private double value;
+
+    //public Vector(double value) {
+    //    this.value = value;  //конструктор будет принимать какое то значение value и заносить это значение
+                             //во внутреннее поле
+    //}
+}
