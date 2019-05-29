ccb3267601616d4c7c27b4f0028ee8dafde41ae8
Evgeniy Yakovets <49934014+jekson0702@users.noreply.github.com>
2019-05-29 21:25:21 +0300

    jd01_08 TaskA

---
+++ src/by/it/yakovets/jd01_08/Matrix.java
@@ -0,0 +1,52 @@
+package by.it.yakovets.jd01_08;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Matrix extends Var {
+    private double[][] value;
+
+    public Matrix(double[][] value) {
+        this.value = Arrays.copyOf(value, value.length);
+    }
+
+    public Matrix(Matrix matrix) {
+        this.value = matrix.value;
+    }
+
+    public Matrix(String strMatrix) {
+        String line = strMatrix.replaceAll("\\s", "");
+        line = line.replaceAll("[\\{}]{2}", "");
+        Pattern pat=Pattern.compile("},\\{");
+        Matcher matcher=pat.matcher(line);
+        int count=0;
+        while (matcher.find()){
+            count++;
+        }
+        String[] masNstr = line.split("},\\{");
+        double[][] mass = new double[masNstr.length][count+1];
+        for (int i = 0; i < masNstr.length; i++) {
+            String arr[] = masNstr[i].split(",");
+            for (int i1 = 0; i1 < arr.length; i1++) {
+                mass[i][i1] = Double.parseDouble(arr[i1]);
+            }
+        }
+        this.value = mass;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{{");
+        String del = "";
+        for (int i = 0; i < value.length; i++) {
+            for (int i1 = 0; i1 < value[0].length; i1++) {
+                sb.append(del).append(value[i][i1]);
+                del = ", ";
+            }
+            del = "},{";
+        }
+        sb.append("}}");
+        return sb.toString();
+    }
+}
