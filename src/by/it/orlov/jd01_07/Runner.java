c414087fbc0270c2ea463ba3399b1599638178f7
dmitrijorlov <MyPassword#4>
2019-05-29 18:24:00 +0300

    jd01_07

--- src/by/it/orlov/jd01_07/Runner.java
+++ src/by/it/orlov/jd01_07/Runner.java
@@ -2,11 +2,6 @@ package by.it.orlov.jd01_07;
 
 public class Runner {
     public static void main(String[] args) {
-        double o = 3.14;
-        String str = "{1.0, 2.0, 5.0";
-        Scalar v = new Scalar(o);
-        //Object v2 = new Vector(str);
-        System.out.println(v);
-        //System.out.println(v2);
+
     }
 }
