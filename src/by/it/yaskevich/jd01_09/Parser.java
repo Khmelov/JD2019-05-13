6f996e3dd147c79e5571fe6ca9f42f7678e03b50
Artem <artem2325@bk.ru>
2019-05-31 21:19:36 +0300

    Задача jd01_09 A

---
+++ src/by/it/yaskevich/jd01_09/Parser.java
@@ -0,0 +1,31 @@
+package by.it.yaskevich.jd01_09;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+    Var calc(String expression) {
+        expression = expression.replaceAll("\\s+", "");
+        String[] part = expression.split(Patterns.OPERATION);
+        Var result = null;
+        if (part.length == 1) {
+            Var.createVar(part[0]);
+        } else if (part.length == 2) {
+            Var one = Var.createVar(part[0]);
+            Var two = Var.createVar(part[1]);
+
+            Pattern pattern = Pattern.compile(Patterns.OPERATION);
+            Matcher matcher = pattern.matcher(expression);
+            if (matcher.find()) {
+                String operation = matcher.group();
+                switch (operation) {
+                    case "+": return one.add(two);
+                    case "-": return one.sub(two);
+                    case "*": return one.mul(two);
+                    case "/": return one.div(two);
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/by/it/yaskevich/jd01_09/Printer.java
@@ -0,0 +1,7 @@
+package by.it.yaskevich.jd01_09;
+
+public class Printer {
+    void print(Var variable) {
+        System.out.println(variable);
+    }
+}
