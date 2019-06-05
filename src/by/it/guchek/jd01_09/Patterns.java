5e3b4964ecceac04fe819d082929b48606909bea
Windows7 <guchi5550@gmail.com>
2019-06-05 21:53:41 +0300

    Задание 9 start

--- src/by/it/guchek/jd01_09/Patterns.java
+++ src/by/it/guchek/jd01_09/Patterns.java
@@ -1,4 +1,10 @@
 package by.it.guchek.jd01_09;
 
 public class Patterns {
+    static final String OPERATION = "[-+/*]";
+    static final String SCALAR ="";  //регулярка для чисел
+    static final String VECTOR ="";  //регулярка для векторов
+    static final String MATRIX ="";  //регулярка для матриц
+
+
 }
