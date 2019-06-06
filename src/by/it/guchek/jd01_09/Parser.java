50f36b104bb0b6f4244e1824747566b10b4ea275
Windows7 <guchi5550@gmail.com>
2019-06-06 20:36:01 +0300

    Задание 9 start

---
+++ src/by/it/guchek/jd01_09/Parser.java
@@ -0,0 +1,30 @@
+package by.it.guchek.jd01_09;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {                //Принимает на вход некоторое выражение и в соответствие с ним
+   Var calc(String expression) {                   //в своем внутреннем методе calc должен вычислить значение готовой переменной
+
+      //2.0*2.0
+      String [] operand = expression.split(Patterns.OPERATION);
+      Var one = Var.createVar(operand[0]);
+      Var two = Var.createVar(operand[1]);
+      if ( one==null || two==null )  //TODO create error
+         return null;
+      Pattern p = Pattern.compile(Patterns.OPERATION);
+      Matcher m = p.matcher(expression);      //выщимливаем знаки
+      if (m.find()){
+         String operation = m.group();
+         switch (operation){
+            case "+": one.add(two);
+            case "-": one.sub(two);
+            case "*": one.mul(two);
+            case "/": one.div(two);
+         }
+      }
+
+      return null;
+
+   }
+}
