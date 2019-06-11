235d4e417f0df41199dced5db0443b7ea5fc7b02
iZaykoff <zaykoffzay@gmail.com>
2019-06-12 02:04:02 +0300

    start jd01_10

---
+++ src/by/it/izaykoff/jd01_10/BeanTester.java
@@ -0,0 +1,36 @@
+package by.it.izaykoff.jd01_10;
+
+import by.it._examples_.jd01_10.sample_service.Init;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+public class BeanTester {
+    public static void main(String[] args)throws Exception {
+
+        Class<Bean> beanClass = Bean.class;
+
+        Bean bean = Bean.class.newInstance();
+        
+
+
+        Method[] methods = beanClass.getMethods();
+        for (Method method : methods) {
+            System.out.println(method.getName());
+        }
+
+//        Object bean=null;
+//        for (Method method : methods) {
+//            if (method.isAnnotationPresent(Param.class))
+//            {
+//             if (bean==null)
+//                 bean=beanClass.newInstance();
+//
+////             method.invoke(bean);
+//             System.out.println(method.invoke(bean));
+//
+//            }
+//        }
+
+    }
+}
