2a9ea248afeb83111f19c12dbe75c37b68ccd01b
NatalliaYakimovich <natashkaby1@gmail.com>
2019-06-12 12:59:04 +0300

    jd01_08+jd01_10start

---
+++ src/by/it/yakimovich/jd01_10/Bean.java
@@ -0,0 +1,14 @@
+package by.it.yakimovich.jd01_10;
+
+public class Bean {
+    double sum(int one, int two){
+        return one+two;
+    }
+    double max(int one, int two){
+          return one>two? one: two;
+    }
+    double min(int one, int two){
+        return one<two? one: two;
+    }
+
+}
---
+++ src/by/it/yakimovich/jd01_10/PrintMath.java
@@ -0,0 +1,28 @@
+package by.it.yakimovich.jd01_10;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+/*
+public class PrintMath {
+    public static void main(String[] args) {
+        Class<Math> aClass = Math.class;
+        Method[] methods=aClass.getDeclaredMethods();
+        StringBuilder name=new StringBuilder();
+        for (Method method:methods) {int modifiers=method.getModifiers();
+            name.setLength(0);
+            if((Modifier.PUBLIC&modifiers) ==Modifier.PUBLIC)
+                stringMethod.append("public");
+            if((Modifier.PRIVATE&modifiers) ==Modifier.PRIVATE)
+                stringMethod.append("private");
+            if((Modifier.STATIC&modifiers) ==Modifier.STATIC)
+                stringMethod.append("static");
+            String returnName=method.getReturnType().getSimpleName();
+            name.append(method.getName());
+            stringMethod.append(returnName).append(' ').append(method.getName()).append(' ');
+
+
+        }
+    }
+}
+*/
