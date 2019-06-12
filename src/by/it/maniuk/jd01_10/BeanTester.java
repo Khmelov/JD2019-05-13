d536cf422fab2ddb6ee7abd4e495404035f1f5d9
DiMan <dfManiuk@gmail.com>
2019-06-12 18:26:18 +0300

    taskС jd01-10 A&B

--- src/by/it/maniuk/jd01_10/BeanTester.java
+++ src/by/it/maniuk/jd01_10/BeanTester.java
@@ -6,12 +6,14 @@ import java.lang.reflect.Method;
 public class BeanTester {
     public static void main(String[] args) throws Exception {
         Class<Bean> beanClass = Bean.class;
-        Method[] methods = beanClass.getMethods();
+        Method[] methods = beanClass.getDeclaredMethods();
         Param anatation;
         for (Method method : methods) {
             if (method.isAnnotationPresent(Param.class)){
-            anatation=method.getAnnotation(Param.class);
+            anatation = method.getAnnotation (Param.class);
+                System.out.println(anatation);
 
+                method.invoke(Param.class);
 
             }
         }
