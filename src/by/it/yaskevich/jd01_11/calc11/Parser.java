873992a48df530298b66a4619b60d12f04e2dac5
Vladimir Tarasenko <vladimirtarasenko33@gmail.com>
2019-06-12 21:22:58 +0300

    Задача jd01_11 A

---
+++ src/by/it/yaskevich/jd01_11/calc11/Parser.java
@@ -0,0 +1,37 @@
+package by.it.yaskevich.jd01_11.calc11;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Parser {
+    Var calc(String expression) {
+        expression = expression.replaceAll("\\s+", "");
+        String[] part = expression.split(Patterns.OPERATION);
+        Var result = null;
+        if (part.length == 1) {
+            Var.createVar(part[0]);
+        } else if (part.length == 2) {
+            //Var one = Var.createVar(part[0]);
+            Var two = Var.createVar(part[1]);
+
+            Pattern pattern = Pattern.compile(Patterns.OPERATION);
+            Matcher matcher = pattern.matcher(expression);
+
+            matcher.find();
+            String operation = matcher.group();
+            if (operation.equals("=")) {
+                Var.save(part[0], two);
+                return two;
+            }
+            Var one = Var.createVar(part[0]);
+
+            switch (operation) {
+                case "+": return one.add(two);
+                case "-": return one.sub(two);
+                case "*": return one.mul(two);
+                case "/": return one.div(two);
+            }
+        }
+        return result;
+    }
+}
