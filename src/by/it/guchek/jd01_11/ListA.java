34b6e90e7526b6a9df60f81f70cfdcdcf2fe6bc8
Windows7 <guchi5550@gmail.com>
2019-06-13 21:51:46 +0300

    Задание jd01_11 A

--- src/by/it/guchek/jd01_11/ListA.java
+++ src/by/it/guchek/jd01_11/ListA.java
@@ -1,11 +1,83 @@
 package by.it.guchek.jd01_11;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class ListA <E> implements List <E> {
+
+    private E[] elements = (E[]) new Object[] {};   // массив обджектов необходимо скастить к типу Е
+
+    // Сделаем так чтобы при удалении элемента длина массива не уменьшалась
+    // храним размер в следующем поле
+
+    private int size=0;     //который изначально равен 0
+
+
+    @Override
+    public boolean add(E e) { //сначала проверим длину
+        if (size==elements.length)
+            elements= Arrays.copyOf(elements, (size*3/2)+1);  //увеличим длину массива по такой формуле как в станд JDK
+        //теперь последний индекс точно больше чем переменная size
+            elements[size]=e;
+            size++;
+
+        return false;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+        if (size==elements.length)
+            elements = Arrays.copyOf(elements, (size * 3 / 2) + 1);  //увеличим длину массива по такой формуле как в станд JDK
+            //теперь последний индекс точно больше чем переменная size
+            System.arraycopy(elements, index, elements, index+1, size-index);
+
+            elements[index]=element;
+            size++;
+
+
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        E delElem = elements[index];
+        //if (size==elements.length)
+        //    elements = Arrays.copyOf(elements, (size * 3 / 2) + 1);  //увеличим длину массива по такой формуле как в станд JDK
+        //теперь последний индекс точно больше чем переменная size
+        System.arraycopy(elements, index+1, elements, index, size-index-1);
+
+        size--;
+        return delElem;
+
+    }
+
+
+    @Override
+    public E get(int index) {
+
+        return elements[index];
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        String delimeter="";
+
+        for (int i = 0; i < size; i++) {
+
+            sb.append(delimeter).append(elements[i]);
+            delimeter=", ";
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    /////
+
 
 
     @Override
@@ -38,17 +110,7 @@ public class ListA <E> implements List <E> {
         return null;
     }
 
-    @Override
-    public boolean add(E e) {
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
+      @Override
     public boolean containsAll(Collection<?> c) {
         return false;
     }
@@ -79,24 +141,10 @@ public class ListA <E> implements List <E> {
     }
 
     @Override
-    public E get(int index) {
-        return null;
-    }
-
-    @Override
     public E set(int index, E element) {
         return null;
     }
 
-    @Override
-    public void add(int index, E element) {
-
-    }
-
-    @Override
-    public E remove(int index) {
-        return null;
-    }
 
     @Override
     public int indexOf(Object o) {
--- src/by/it/guchek/jd01_11/Runner.java
+++ src/by/it/guchek/jd01_11/Runner.java
@@ -5,10 +5,27 @@ import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
+        List <String> mylist = new ListA<>(); //my
         List <String> list = new ArrayList<>(); //настоящий
-        list.add("one");list.add("two");list.add("three");
-        System.out.println(list);
-        System.out.println(list.get(1));
+
+        //проверим добавление
+        mylist.add("one"); list.add("one");
+        mylist.add("two"); list.add("two");
+        mylist.add("four");list.add("four");
+        System.out.println("mylist:"+mylist+"\n  list:"+list);
+        mylist.add(2, "three"); list.add(2,"three");
+        mylist.add(0, "Start"); list.add(0,"Start");
+        System.out.println("mylist:"+mylist+"\n  list:"+list);
+
+        // проверим удаление
+        mylist.remove(2); list.remove(2);
+        System.out.println("mylist:"+mylist+"\n  list:"+list);
+
+        // проверим чтение
+        mylist.get(3); list.get(3);
+        System.out.println("mylist:"+mylist.get(3)+"\n  list:"+list.get(3));
+
+
 
     }
 
