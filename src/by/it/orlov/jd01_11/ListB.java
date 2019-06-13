3338ecf0b9af84c7e77eea1b2163305a0ebb7ca7
dmitrijorlov <MyPassword#4>
2019-06-13 22:05:15 +0300

    11

---
+++ src/by/it/orlov/jd01_11/ListB.java
@@ -0,0 +1,171 @@
+package by.it.orlov.jd01_11;
+
+import java.util.*;
+import java.util.function.UnaryOperator;
+
+public class ListB<T> implements List<T> {
+    private T[] elements = (T[]) new Object[100];
+    private int size = 0;
+    @Override
+    public void replaceAll(UnaryOperator<T> operator) {
+
+    }
+    @Override
+    public String toString() {
+        StringBuilder txt = new StringBuilder("[");
+        String delimiter = "";
+        for (int i = 0; i < size; i++) {
+            txt.append(delimiter).append(elements[i]);
+            delimiter = ", ";
+        }
+        txt.append("]");
+        return txt.toString();
+    }
+
+    @Override
+    public T remove(int index) {
+        T element = elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
+        return element;
+    }
+
+
+
+    @Override
+    public boolean add(T t) {
+        if(size==elements.length){
+            System.arraycopy(elements,0,elements,0,elements.length/2*3+1);
+        }
+        elements[size++]=t;
+        return true;
+    }
+
+
+    @Override
+    public void sort(Comparator<? super T> c) {
+
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        T previous = elements[index];
+        elements[index] = element;
+        return previous;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+    @Override
+    public T get(int index) {
+        return elements[index];
+    }
+
+
+    @Override
+    public void add(int index, T element) {
+
+        T bufer= elements[size];
+        for (int i = size; i > index; i--) {
+            elements[i]=elements[i-1];
+
+        }
+        add(bufer);
+        elements[index]=element;
+
+    }
+
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
