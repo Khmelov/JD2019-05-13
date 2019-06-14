f58e2c55fd0b3f3fe3d1cfca3cd4b5f3b31efb35
RudnitskTimofei <3761544Radiohead>
2019-06-14 20:34:05 +0300

    jd01_11, calc

---
+++ src/by/it/trudnitski/calc/Printer.java
@@ -0,0 +1,24 @@
+package by.it.trudnitski.calc;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class Printer {
+    void print(Var variable){
+        if(variable!=null)
+        System.out.println(variable);
+    }
+    void printvar(Map<String,Var> result){
+        Set<Map.Entry<String,Var>> enSet=result.entrySet();
+        Iterator<Map.Entry<String,Var>> it=enSet.iterator();
+        while(it.hasNext()){
+            Map.Entry<String,Var> elem=it.next();
+            System.out.println(elem.getKey()+"="+elem.getValue());
+        }
+    }
+    void sortVar(){
+        Map<String,Var> sort=Var.getVars();
+        printvar(sort);
+    }
+}
