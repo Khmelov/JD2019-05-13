8910846eae2eb2d6852055e78069bc53461b9883
Windows7 <guchi5550@gmail.com>
2019-06-16 00:36:09 +0300

    Задание jd01_12А2

--- src/by/it/guchek/jd01_12/TaskA2.java
+++ src/by/it/guchek/jd01_12/TaskA2.java
@@ -1,10 +1,30 @@
 package by.it.guchek.jd01_12;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TaskA2 {
 
-    private List<Integer> list = new ArrayList<>();
-    private List <Integer> list2 = new ArrayList<>();
+    private static Set<Integer> getCross(Set<Integer> one, Set<Integer> two){
+
+        Set<Integer> result= new HashSet<>(one);
+        result.retainAll(two);   //retain - пересечение множеств
+        return result;
+    }
+
+    private static Set<Integer> getUnion(Set<Integer> one, Set<Integer> two){
+
+        Set<Integer> result= new HashSet<>(one);
+        result.addAll(two);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeSet<Integer> treeSet = new TreeSet<>(Arrays.asList(1,2,3,4,4,6,7,7,9,10));
+        HashSet<Integer> hashSet = new HashSet<>(Arrays.asList(3,4,4,5,5,6,7,7,9));
+        System.out.println(getCross (treeSet, hashSet));
+        System.out.println(getUnion (treeSet, hashSet));
+
+    }
+
+
 }
---
+++ src/by/it/guchek/jd01_12/TaskA3.java
@@ -0,0 +1,4 @@
+package by.it.guchek.jd01_12;
+
+public class TaskA3 {
+}
