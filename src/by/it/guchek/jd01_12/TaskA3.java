a62b7c3c9f7474b25a9c37c1f805675d3cd00c53
Windows7 <guchi5550@gmail.com>
2019-06-16 01:42:37 +0300

    Задание jd01_12А3

--- src/by/it/guchek/jd01_12/TaskA3.java
+++ src/by/it/guchek/jd01_12/TaskA3.java
@@ -1,4 +1,35 @@
 package by.it.guchek.jd01_12;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class TaskA3 {
+    public static void main(String[] args) {
+        List<Integer> array = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        String stroke;
+        int poz=0;
+        while (!(stroke = scan.next()).equals("end") ){
+
+            //Integer value = Integer.valueOf(stroke);
+            /*if (value>0)
+                array.add(poz++,value);
+            else if (value==0)
+                array.add(poz,0);
+            else array.add(value);}*/
+
+            if (stroke.equals("end"))
+                break;
+            Integer element=Integer.parseInt(stroke);
+            if (element<0)
+                array.add(element);
+            else if (element==0)
+                array.add(poz,element);
+            else
+                array.add(poz++,element);
+        }
+
+        System.out.println(array);
+    }
 }
