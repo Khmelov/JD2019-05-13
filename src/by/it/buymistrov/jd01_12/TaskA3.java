b539bef050a015d3c33802a26041f93ffdc541e3
alexonchik <alex-byi@mail.ru>
2019-06-15 21:19:43 +0300

    jd01_12

---
+++ src/by/it/buymistrov/jd01_12/TaskA3.java
@@ -0,0 +1,30 @@
+package by.it.buymistrov.jd01_12;
+
+import java.util.Scanner;
+
+public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+
+        for (; ; ) {
+
+
+            String next = sc.next();
+            if (next.equals("end")){
+                break;
+            }
+
+//            Integer element = Integer.parseInt(next);
+//            if (element<0){
+//                list.add(element);
+//            } else  if (element==0){
+//                list.add(posZero,element);
+//            } else if (element>0){
+//                list.add(posZero++,element);
+//            }
+
+        }
+
+    }
+}
