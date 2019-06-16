e08e5b0b5a67c0a56b12da5a517f897d300230e6
Windows7 <guchi5550@gmail.com>
2019-06-16 17:05:22 +0300

    Задание jd01_12B1-2start

---
+++ src/by/it/guchek/jd01_12/TaskB2.java
@@ -0,0 +1,42 @@
+package by.it.guchek.jd01_12;
+
+import java.util.*;
+
+public class TaskB2 {
+    static List <String> listArr = new ArrayList<>();
+    static List <String> listLink = new LinkedList<>();
+    static String process(ArrayList<String> peoples){
+        return "0";
+    };
+    static String process(LinkedList<String> peoples){
+        return "1";
+    };
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Scanner scan1 = new Scanner(System.in);
+        Integer countPeople = scan.nextInt();
+
+        String stroke;
+
+        for (Integer i = 0; i < (int)(countPeople); i++) {
+
+            stroke = scan1.nextLine();
+            listArr.add(stroke);
+            listLink.add(stroke);
+        }
+
+        System.out.println(listArr);
+        System.out.println(listLink);
+
+        ListIterator<String> strListIt = listLink.listIterator();
+        while (strListIt.hasNext()){
+            String next = strListIt.next();
+            strListIt.remove();
+        }
+        System.out.println(listLink);
+        ListIterator<String> strArrListIt = listArr.listIterator();
+
+
+    }
+}
