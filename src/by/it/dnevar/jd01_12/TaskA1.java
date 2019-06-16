50566f7074fa80bb4587aa370af60c2c5d7da650
Dima <nevar.dima@gmail.com>
2019-06-16 14:00:25 +0300

    add 01_12

--- src/by/it/dnevar/jd01_12/TaskA1.java
+++ src/by/it/dnevar/jd01_12/TaskA1.java
@@ -1,16 +1,32 @@
 package by.it.dnevar.jd01_12;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
 public class TaskA1 {
-    private static List<Integer> grades = new ArrayList<>();
+    private List<Integer> grades = new ArrayList<>();
+    private static Random generator = new Random(15);
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-
+        TaskA1 instance = new TaskA1();
+        for (int i = 0; i < 20; i++) {
+            Integer grade = 1+generator.nextInt(10);
+            instance.grades.add(grade);
         }
+        System.out.println(instance.grades);
+        instance.clearBad(instance.grades);
+        System.out.println(instance.grades);
 
+    }
+
+    private void clearBad (List<Integer> grades){
+        Iterator<Integer> iterator = grades.iterator();
+        while(iterator.hasNext()){
+            if(iterator.next()<4){
+                iterator.remove();
+            }
+        }
     }
 }
