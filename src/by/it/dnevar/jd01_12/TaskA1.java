492a0078871d35330377c81d09a5db1733dea70f
Dima <nevar.dima@gmail.com>
2019-06-16 12:43:00 +0300

    add 01_12

---
+++ src/by/it/dnevar/jd01_12/TaskA1.java
@@ -0,0 +1,16 @@
+package by.it.dnevar.jd01_12;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class TaskA1 {
+    private static List<Integer> grades = new ArrayList<>();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+    }
+}
