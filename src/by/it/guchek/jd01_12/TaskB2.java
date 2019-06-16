7fc34db85b36bf5cf36cc107a759c21d0a237d0c
Windows7 <guchi5550@gmail.com>
2019-06-16 22:30:30 +0300

    Задание jd01_12B3

--- src/by/it/guchek/jd01_12/TaskB2.java
+++ src/by/it/guchek/jd01_12/TaskB2.java
@@ -3,14 +3,51 @@ package by.it.guchek.jd01_12;
 import java.util.*;
 
 public class TaskB2 {
-    static List <String> listArr = new ArrayList<>();
-    static List <String> listLink = new LinkedList<>();
-    static String process(ArrayList<String> peoples){
-        return "0";
-    };
-    static String process(LinkedList<String> peoples){
-        return "1";
-    };
+    static ArrayList<String> listArr = new ArrayList<>();
+    static LinkedList<String> listLink = new LinkedList<>();
+
+    static String process(ArrayList<String> peoples) {
+        boolean second = false;
+
+        while (peoples.size() > 1) {
+            for (int i = 0; i < peoples.size(); i++) {
+                if (second) {
+                    peoples.remove(i);
+                    System.out.println(peoples);
+                    second = false;
+                    --i;
+                } else {
+                    second = true;
+                }
+            }
+        }
+
+        return peoples.get(0);
+    }
+
+    ;
+
+    static String process(LinkedList<String> peoples) {
+        boolean second = false;
+
+        while (peoples.size() > 1) {
+            ListIterator<String> strListIt = peoples.listIterator();
+
+            while (strListIt.hasNext()) {
+                strListIt.next();
+                if (second) {
+                    strListIt.remove();
+                    second = false;
+                } else {
+                    second = true;
+                }
+            }
+        }
+
+        return peoples.get(0);
+    }
+
+    ;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -19,30 +56,12 @@ public class TaskB2 {
 
         String stroke;
 
-        for (Integer i = 0; i < (int)(countPeople); i++) {
+        for (Integer i = 0; i < (int) (countPeople); i++) {
 
             stroke = scan1.nextLine();
             listArr.add(stroke);
             listLink.add(stroke);
         }
-
-        System.out.println(listArr);
-        System.out.println(listLink);
-        boolean second=false;
-
-        ListIterator<String> strListIt = listLink.listIterator();
-        while (strListIt.hasNext()){
-            String  next = strListIt.next();
-            System.out.println(next);
-            //System.out.println(next);
-            //{if (next%2==0)
-            if (second){strListIt.remove();
-                    second=false;}
-        else second=true;
-        }
-        System.out.println(listLink);
-        ListIterator<String> strArrListIt = listArr.listIterator();
-
 
     }
 }
