bb6967ec2d0eb20b2a6af78c92c1476430280ba1
alexonchik <alex-byi@mail.ru>
2019-06-17 18:31:04 +0300

    jd01_11 без костылей

--- src/by/it/buymistrov/jd01_12/TaskC1.java
+++ src/by/it/buymistrov/jd01_12/TaskC1.java
@@ -7,11 +7,11 @@ import java.util.*;
 public class TaskC1 {
 
     private static Random generator = new Random();
-    private static TreeSet<String> setT = new TreeSet<>();
-    private static Map<String, String> mapV = new HashMap<>();
-    private static Set<Map.Entry<String, String>> set = mapV.entrySet();
-    private static Set<String> set1 = new HashSet<>();
-    private static BiMap<String, String> myBiMap = HashBiMap.create();
+    private static TreeSet<String> nameSet = new TreeSet<>();
+    private static Map<String, String> nameMap = new HashMap<>();
+    private static Set<Map.Entry<String, String>> set = nameMap.entrySet();
+    private static Set<String> resultSet = new HashSet<>();
+    private static BiMap<String, String> resultBiMap = HashBiMap.create();
 
     public static void main(String[] args) {
 
@@ -21,29 +21,29 @@ public class TaskC1 {
         String name;
         while (!(name = sc.nextLine()).equals("end")) {
             int gen = 1 + generator.nextInt(99) + generator.nextInt(9999);
-            mapV.put(Integer.toString(gen), name);
+            nameMap.put(Integer.toString(gen), name);
         }
 
         //сортировка
         for (Map.Entry<String, String> key : set) {
-            setT.add(key.getKey());
+            nameSet.add(key.getKey());
         }
 
-        //вывод с дубликатами
-        for (String s : setT) {
-            System.out.println(mapV.get(s) + " - " + s);
-            set1.add(mapV.get(s));
-            myBiMap.put(mapV.get(s), s);
+        //вывод с сортировкой и дубликатами
+        for (String s : nameSet) {
+            System.out.println(nameMap.get(s) + " - " + s);
+            resultSet.add(nameMap.get(s));
+            resultBiMap.put(nameMap.get(s), s);
         }
 
 
         System.out.println();
 
-        //вывод результата
+        //вывод результата без дубликатов
         // НЕ БУДЕТ РАБОТАТЬ БЕЗ КОЛЛЕКЦИЙ ОТ ГУГЛА!(но это не точно)
         System.out.println("Result");
-        for (String s : set1) {
-            System.out.println(s + " - " + myBiMap.get(s));
+        for (String s : resultSet) {
+            System.out.println(s + " - " + resultBiMap.get(s));
         }
 
 
