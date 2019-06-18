2147ef1be25eed049a4ccccbe3208cc76a412f7b
alexey-adamovich <alex.adamovich83@gmail.com>
2019-06-18 23:13:08 +0300

    jd01_13

--- src/by/it/aadamovich/jd01_13/TaskC.java
+++ src/by/it/aadamovich/jd01_13/TaskC.java
@@ -5,28 +5,5 @@ import java.util.Scanner;
 
 public class TaskC {
 
-    public static void main(String[] args) throws InterruptedException {
-        readData();
-    }
-
-    private static void readData() throws InterruptedException {
-
-        Scanner sc = new Scanner(System.in);
-        LinkedList<Double> list = new LinkedList<>();
-
-        int count = 0;
-
-        while (true) {
-            try {
-                list.addFirst(Double.valueOf(sc.nextLine()));
-            } catch (NumberFormatException e) {
-                Thread.sleep(100);
-                for (Double element : list) {
-                    System.out.print(element + " ");
-                }
-                System.out.println();
-                if (++count == 5) throw e;
-            }
-        }
-    }
+    
 }
