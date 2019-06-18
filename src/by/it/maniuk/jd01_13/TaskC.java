cc04f1705b6082e06e80dd2ee69bf33172a1ccaa
DiMan <dfManiuk@gmail.com>
2019-06-18 23:12:24 +0300

    jd01-13

--- src/by/it/maniuk/jd01_13/TaskC.java
+++ src/by/it/maniuk/jd01_13/TaskC.java
@@ -6,41 +6,37 @@ import java.util.*;
 
 public class TaskC {
     public static void main(String[] args) throws Exception {
-       // Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        readeData();
+        readData();
 
     }
 
-    static void readeData() throws Exception {
+    static void readData() throws Exception {
         List<Double> list = new ArrayList<>();
         int count = 0;
-        while (count != 5) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (; ; ) {
             try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-               for (; ;) {
-                   String vvod = reader.readLine();
-                   String[] tok = vvod.split(" ");
-                   for (String value : tok) {
-                       double s = Double.parseDouble(value);
-                       list.add(s);
-                   }
-               }
-            } catch (Exception exe) {
-                count++;
+
+                list.add(Double.parseDouble(reader.readLine()));
+
+            } catch (NumberFormatException exe) {
+
                 Thread.sleep(100);
                 for (int i = list.size() - 1; i >= 0; i--) {
                     System.out.print(list.get(i) + " ");
                 }
-
-
+                System.out.println();
+                count++;
+                if (count == 5) throw exe;
             }
-        }
 
-        throw new Exception();
+        }
 
     }
 }
+
 
 
 
