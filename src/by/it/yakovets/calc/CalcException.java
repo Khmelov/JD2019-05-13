b25ad1f1c968f75dd8ee6c1cda011dbfb8770ec7
Evgeniy Yakovets <49934014+jekson0702@users.noreply.github.com>
2019-06-19 17:54:05 +0300

    jd01_13 TaskC

--- src/by/it/yakovets/calc/CalcException.java
+++ src/by/it/yakovets/calc/CalcException.java
@@ -14,9 +14,13 @@ public class CalcException extends Exception {
         super("ERROR:"+message, cause);
     }
 
+    protected CalcException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super("ERROR:"+message, cause, enableSuppression, writableStackTrace);
+    }
+
     public CalcException(Throwable cause) {
         super("ERROR:"+cause);
     }
 
-    }
+}
 
