e7d3016f3a6c40b17b3d38c83259fb47fb4d802d
DiMan <dfManiuk@gmail.com>
2019-06-19 18:32:39 +0300

    jd01-13

--- src/by/it/maniuk/jd01_13/TaskC.java
+++ src/by/it/maniuk/jd01_13/TaskC.java
@@ -5,7 +5,7 @@ import java.io.*;
 import java.util.*;
 
 public class TaskC {
-    static Scanner reader;
+    private static Scanner reader;
 
     public static void main(String[] args) throws Exception {
         reader = new Scanner(System.in);
