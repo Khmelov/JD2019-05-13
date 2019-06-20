078add2cbafcf6b0f4fbbac24175f21fe2eabc08
Mikhail Yaskevich <job.mikhail@gmail.com>
2019-06-20 18:54:25 +0200

    Задание jd01_14

--- src/by/it/yaskevich/jd01_14/TaskA.java
+++ src/by/it/yaskevich/jd01_14/TaskA.java
@@ -2,81 +2,90 @@ package by.it.yaskevich.jd01_14;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class TaskA {
     public static void main(String[] args) {
-        String path = getPath();
-        String fileName = getFilePath("dataTaskA.bin");
-        System.out.println(fileName);
+        String fileName = getFilePath(TaskA.class, "dataTaskA.bin");
 
-        try (OutputStream out = new FileOutputStream(fileName);
-             BufferedOutputStream bos = new BufferedOutputStream(out);
-             DataOutputStream dos = new DataOutputStream(bos);) {
-
-            for (int i = 0; i < 20; i++) {
-                dos.writeInt((int)(Math.random() * 100));
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        writeData(fileName);
 
         List<Integer> list = new ArrayList<>(20);
-        try (DataInputStream dis = new DataInputStream(
-                    new BufferedInputStream(
-                            new FileInputStream(fileName)))) {
+        readData(fileName, list);
+        printData(list);
+        printDataCopy(getFilePath(TaskA.class, "resultTaskA.txt"), list);
 
-            while (dis.available() > 0) {
-                list.add(dis.readInt());
+        writeTextData(getFilePath(TaskA.class, "dataTaskA.txt"), list);
+    }
+
+    private static void printDataCopy(String fileName, List<Integer> list) {
+        double sum = 0;
+        try (PrintWriter out = new PrintWriter(new FileWriter(fileName))) {
+            for (Integer value : list) {
+                out.write(value + " ");
+                sum += value;
             }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            out.write("\navg=" + sum/list.size());
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        double sum = 0;
-        for (Integer value : list) {
-            System.out.print( value + " ");
-            sum += value;
-        }
-        System.out.println("avg=" + sum/list.size());
-
-
-        try (
-            PrintWriter out = new PrintWriter(
-                    new FileWriter(getFilePath("dataTaskA.txt")))){
-
+    private static void writeTextData(String fileName, List<Integer> list) {
+        try (PrintWriter out = new PrintWriter(new FileWriter(fileName))){
             for (Integer value : list) {
                 out.print(value);
             }
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String getPath() {
-        String root = System.getProperty("user.dir");
-        System.out.println(root);
-
-        String separator = File.separator;
-        String name = TaskA.class.getName();
-        String simpleName = TaskA.class.getSimpleName();
-        String path = name.replace(simpleName, "").replace(".", separator);
-        path = root + separator + "src" + separator + path;
-        return path;
+    private static void printData(List<Integer> list) {
+        double sum = 0;
+        for (Integer value : list) {
+            System.out.print(value + " ");
+            sum += value;
+        }
+        System.out.println();
+        System.out.println("avg=" + sum/list.size());
     }
 
-    private static String getFilePath(String fileName) {
-        return getPath() + fileName;
+    private static void readData(String fileName, List<Integer> list) {
+        try (DataInputStream dis = new DataInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(fileName)))) {
+            while (dis.available() > 0) {
+                list.add(dis.readInt());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeData(String fileName) {
+        try (DataOutputStream dos = new DataOutputStream(
+                new BufferedOutputStream(
+                        new FileOutputStream(fileName)))) {
+            for (int i = 0; i < 20; i++) {
+                dos.writeInt((int)(Math.random() * 100));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getPath(Class<TaskA> aClass) {
+        String root = System.getProperty("user.dir");
+        String separator = File.separator;
+        String name = aClass.getName();
+        String simpleName = aClass.getSimpleName();
+        return root + separator + "src" + separator +
+                name.replace(simpleName, "").replace(".", separator);
+    }
+
+    private static String getFilePath(Class<TaskA> aClass, String fileName) {
+        return getPath(aClass) + fileName;
     }
 
 }
