c78e36679bfbf350eb01bce1ed5a4ebc6fca52a8
RudnitskTimofei <3761544Radiohead>
2019-06-21 02:53:24 +0300

    jd01_14, and jd01_11 taskB.

--- src/by/it/trudnitski/jd01_12/TaskB2.java
+++ src/by/it/trudnitski/jd01_12/TaskB2.java
@@ -3,20 +3,22 @@ package by.it.trudnitski.jd01_12;
 import java.util.*;
 
 public class TaskB2 {
-    private static Integer[]arr={1, 1, 3, 1, 3, 5, 7, 1, 3, 5, 7, 9, 11, 13, 15, 1};
-    private static List<?> arrayList =new ArrayList<>(Arrays.asList(arr));
-    private static List<?> linkedList = new LinkedList<>(Arrays.asList(arr));
+//    private static Integer[]arr={1, 1, 3, 1, 3, 5, 7, 1, 3, 5, 7, 9, 11, 13, 15, 1};
+//    private static List<?> arrayList =new ArrayList<>(Arrays.asList(arr));
+//    private static List<?> linkedList = new LinkedList<>(Arrays.asList(arr));
 
 
     static String process(ArrayList<String> peoples) {
-        Iterator<String> iterator = peoples.iterator();
+        boolean answer=false;
         if (peoples.size() == 1) return peoples.toString();
         while (peoples.size() > 1) {
+            Iterator<String> iterator = peoples.iterator();
             while (iterator.hasNext()) {
                     iterator.next();
-                    if (iterator.hasNext()) {
+                    if (answer) {
                         iterator.remove();
                     }
+                    answer=!answer;
                 }
             }
         return peoples.get(0);
@@ -25,14 +27,16 @@ public class TaskB2 {
 
 
         static String process (LinkedList < String > peoples) {
-            Iterator<String> iterator = peoples.iterator();
+        boolean answer=false;
             if (peoples.size() == 1) return peoples.toString();
             while (peoples.size() > 1) {
+                Iterator<String> iterator = peoples.iterator();
                 while (iterator.hasNext()) {
                     iterator.next();
-                    if (iterator.hasNext()) {
+                    if (answer) {
                         iterator.remove();
                     }
+                    answer=!answer;
                 }
             }
             return peoples.get(0);
@@ -41,8 +45,16 @@ public class TaskB2 {
 
         public static void main (String[]args){
 
+            ArrayList<String> arrayList = new ArrayList<>(10);
+            LinkedList<String> linkedList = new LinkedList<>();
+            for (int i = 1; i <= 10; i++) {
+                arrayList.add("n" + i);
+                linkedList.add("n" + i);
+            }
+
             System.out.println(arrayList);
-            System.out.println(process((ArrayList<String>) arrayList));
+            System.out.println(process(arrayList));
+            System.out.println(process(linkedList));
         }
 
     }
--- src/by/it/trudnitski/jd01_12/TaskB3.java
+++ src/by/it/trudnitski/jd01_12/TaskB3.java
@@ -6,25 +6,26 @@ public class TaskB3 {
 
 
     static String process(ArrayList<String> peoples) {
-        Iterator<String> iterator = peoples.iterator();
+        boolean answer = false;
         if (peoples.size() == 1) return peoples.toString();
         while (peoples.size() > 1) {
+            Iterator<String> iterator = peoples.iterator();
             while (iterator.hasNext()) {
                 iterator.next();
-                if (iterator.hasNext()) {
+                if (answer) {
                     iterator.remove();
                 }
+                answer = !answer;
             }
         }
         return peoples.toString();
     }
 
 
-
-    static String process (LinkedList < String > peoples) {
-        while(peoples.size()>1) {
+    static String process(LinkedList<String> peoples) {
+        while (peoples.size() > 1) {
             for (int i = 0; i < peoples.size(); i++) {
-                    peoples.remove(i%2);
+                peoples.remove(i % 2);
             }
         }
 
@@ -32,35 +33,33 @@ public class TaskB3 {
     }
 
 
-    public static void main (String[]args){
-        Timer t=new Timer();
-        Integer []arr = new Integer[4097];
-        for (int i = 1; i <=4096; i++) {
-            arr[i]=i;
+    public static void main(String[] args) {
+        Timer t = new Timer();
+        Integer[] arr = new Integer[4097];
+        for (int i = 1; i <= 4096; i++) {
+            arr[i] = i;
         }
-        for (Integer integer : arr) {
-
-            System.out.println(integer);
-        }
-        List<?> arrayList =new ArrayList<>(Arrays.asList(arr));
+        List<?> arrayList = new ArrayList<>(Arrays.asList(arr));
         List<?> linkedList = new LinkedList<>(Arrays.asList(arr));
-        System.out.println("ИСкомый элемент"+process((ArrayList<String>) arrayList));
+        System.out.println("ИСкомый элемент" + process((ArrayList<String>) arrayList));
         System.out.println(t);
-        System.out.println("ИСкомый элемент"+process((LinkedList<String>) linkedList));
+        System.out.println("ИСкомый элемент" + process((LinkedList<String>) linkedList));
         System.out.println(t);
 
     }
 
-    public static class Timer{
+    public static class Timer {
         private long iniTimer;
         private Double Delta;
-        public Timer(){
-            iniTimer=System.nanoTime();
+
+        public Timer() {
+            iniTimer = System.nanoTime();
         }
-        public String toString(){
-            Delta=(double)(System.nanoTime()-iniTimer)/1000;
-            iniTimer=System.nanoTime();
-            return "Прошло "+ Delta.toString()+ " Микросекунд";
+
+        public String toString() {
+            Delta = (double) (System.nanoTime() - iniTimer) / 1000;
+            iniTimer = System.nanoTime();
+            return "Прошло " + Delta.toString() + " Микросекунд";
         }
     }
 
