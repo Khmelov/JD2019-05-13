8b82c1f26c4efc485df88519dd9e406bd2d556f1
Windows7 <guchi5550@gmail.com>
2019-06-24 00:25:12 +0300

    jd01_15-Bstart

--- src/by/it/guchek/jd01_15/TaskB.java
+++ src/by/it/guchek/jd01_15/TaskB.java
@@ -1,4 +1,37 @@
 package by.it.guchek.jd01_15;
 
+import java.io.File;
+
 public class TaskB {
+    //этот комментарий надо удалить
+    // этот комментарий надо удалить
+    /*бла бла бла
+    бла бла
+     */
+    /*бла бла бла
+    бла бла
+     */
+    /**
+     * Это Джавадок
+     * Это Джавадок
+    */
+
+    private static String getPath(Class<?> aClass) {
+        String root = System.getProperty("user.dir");
+        String name = aClass.getName();
+        String simpleName = aClass.getSimpleName();
+        String separator = File.separator;
+        String path = name.replace(simpleName, "").replace(".", separator);
+        path = root + separator + "src" + separator + path;
+        return path;
+    }
+
+    static String fileName = getPath(TaskB.class)+"TaskB.java";
+    public static void main(String[] args) {
+
+        StringBuilder sb= new StringBuilder();
+        
+
+
+    }
 }
