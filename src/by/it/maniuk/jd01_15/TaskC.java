f9018d8b6745b35c09cdcf586c6279c0d76c519f
DiMan <dfManiuk@gmail.com>
2019-06-23 21:59:02 +0300

    jd01-15

--- src/by/it/maniuk/jd01_15/TaskC.java
+++ src/by/it/maniuk/jd01_15/TaskC.java
@@ -2,56 +2,56 @@ package by.it.maniuk.jd01_15;
 
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class TaskC {
+    static File dir;
 
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args)  {
-        File dir = new File("D:/JD2019-05-13/src/by/it/maniuk/");
-
+        dir = new File("D:/JD2019-05-13/src/by/it/maniuk/jd01_15/");
+        dir = dir.getCanonicalFile();
         Scanner scanner = new Scanner(System.in);
         String s;
         System.out.println("by/it/maniuk/jd01_15/");
         while (!( s = scanner.nextLine()).equals("end")){
 
-
             if (s.contains("cd")){
-                pathFromFile(s);
+                s =  s.trim();
+               String[] line = s.split(" ");
+               if (line[0].equals("cd..") ){
+                dir = new File(dir.getParent());
+                System.out.println(dir.getAbsolutePath());
+                getContents(dir);}
+                else  {
+
+                    StringBuilder sb = new StringBuilder(dir.getAbsolutePath());
+                    sb.append('/').append(line[1]);
+                    String list = sb.toString();
+                   dir = new File(String.valueOf(list));
+                   getContents(dir);
+               }
             }
             if (s.contains("dir")){
                 getContents(dir);
             }
+
         }
     }
 
-    private static void pathFromFile(String s) {
-       s = s.replaceAll("cd", "").trim();
-        String txt = getFilePath(TaskC.class,s);
-        File file = new File(txt);
-
-        Path path = Paths.get(txt);
-        System.out.print(" " +path.getFileName() + "" +file.getTotalSpace());
-
-    }
     private static void getContents(File dir) {
 
         for (File item : Objects.requireNonNull(dir.listFiles())) {
             if (item.isDirectory()) {
-
                 SimpleDateFormat sdf =new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
                 System.out.printf("%-10s   <DIR>   %10d     %-10s\n", sdf.format(item.lastModified()),getFileDir(item), item.getName());
-
             } else {
                 SimpleDateFormat sdf =new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-                System.out.printf("%-10s                     %-10s\n", sdf.format(item.lastModified()), item.getName());
-
+                System.out.printf("%-10s            %10d         %-10s\n", sdf.format(item.lastModified()),item.length() ,item.getName());
             }
-
         }
     }
     private static int getFileDir(File item) {
@@ -59,19 +59,7 @@ public class TaskC {
         list =item.list();
         return list.length;
     }
-    
-    private static String getFilePath(Class<TaskC> aClass, String filename) {
-        return getPath(aClass) + filename;
-    }
 
-    private static String getPath(Class<TaskC> aClass) {
-        String root = System.getProperty("user.dir");
-        String name = TaskC.class.getName();
-        String simpleName = TaskC.class.getSimpleName();
-        String separator = File.separator;
-        String path = name.replace(simpleName, "").replace(".", separator);
 
-        path = root + separator + "src" + separator + path;
-        return path;
-    }
+
 }
