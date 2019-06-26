2c7f1ad756e812d01227780a2be65f09c53a1e03
Mikhail Yaskevich <job.mikhail@gmail.com>
2019-06-26 12:54:19 +0200

    Задание jd02_01

--- src/by/it/yaskevich/jd02_01/Buyer.java
+++ src/by/it/yaskevich/jd02_01/Buyer.java
@@ -1,35 +1,78 @@
 package by.it.yaskevich.jd02_01;
 
-public class Buyer extends Thread /*implements IBuyer*/ {
-    void enterToMarket() {
-        System.out.println("enter to the market");
+public class Buyer extends Thread implements IBuyer, IUseBasket {
+
+    private boolean pensioner = false;
+
+    Buyer(int number) {
+        super("Buyer №" + number);
     }
 
-    public Buyer(int number) {
-        super("Buyer №" + number);
+    @Override
+    public String toString() {
+        return getName() + (isPensioner() ? " (пенсионер)" : "");
     }
 
     @Override
     public void run() {
         enterToMarket();
+        takeBasket();
         chooseGoods();
         goOut();
-
-    }
-
-    private void goOut() {
-        System.out.println("");
     }
 
     @Override
-    public String toString() {
-        return getName();
+    public void enterToMarket() {
+        Market.shoppingBuyers.add(this);
+        printState(this + " вошел в магазин");
     }
 
-    private void chooseGoods() {
-        System.out.println("start");
+    @Override
+    public void chooseGoods() {
+        printState(this + " начал выбирать товары");
+        int countGoods = Utilities.rnd(1, 4);
+        for (int i = 0; i < countGoods; i++) {
+            Utilities.sleep(Utilities.rnd(makeDelay(500), makeDelay(2000)));
+            printState(this + " выбрал " + Goods.getRandomGood());
+            putGoodsToBasket();
+        }
+        printState(this + " завершил выбирать(" + countGoods + ")");
+    }
 
+    @Override
+    public void goOut() {
+        Market.shoppingBuyers.remove(this);
+        printState(this + " вышел из магазина");
 
-        System.out.println("stop");
+    }
+
+    @Override
+    public void takeBasket() {
+        Utilities.sleep(Utilities.rnd(makeDelay(100), makeDelay(200)));
+        printState(this + " взял корзину");
+    }
+
+    @Override
+    public void putGoodsToBasket() {
+        Utilities.sleep(Utilities.rnd(makeDelay(100), makeDelay(200)));
+        printState(this + " положил товар в корзину");
+    }
+
+    private boolean isPensioner() {
+        return pensioner;
+    }
+
+    void setPensioner(boolean pensioner) {
+        this.pensioner = pensioner;
+    }
+
+    private int makeDelay(int value) {
+        return (int)(value * (isPensioner() ? 1.5 : 1));
+    }
+
+    private void printState(String text) {
+        if (Market.printStatesBuyer) {
+            System.out.println(text);
+        }
     }
 }
