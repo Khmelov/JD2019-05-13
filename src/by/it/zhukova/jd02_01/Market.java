b830530305dd80db52660510e077e58b8a5154ac
IrinaZhukova1210 <irinazhukova1210@gmail.com>
2019-06-26 23:06:08 +0300

    Task jd02_01 A-C completed

---
+++ src/by/it/zhukova/jd02_01/Market.java
@@ -0,0 +1,58 @@
+package by.it.zhukova.jd02_01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Market {
+    public static void main(String[] args) {
+        List<Buyer> buyers = new ArrayList<>();
+        System.out.println("Market opened");
+        MarketOpen(buyers);
+        for (Buyer buyer : buyers) {
+                try {
+                    buyer.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        System.out.println("Market closed");
+    }
+
+    private static void MarketOpen(List<Buyer> buyers) {
+        int countBuyer=0;
+        boolean pensioneer;
+        int count =0;
+        for (int min = 0; min < 2; min++) {
+            for (int time = 0; time < 60; time++) {
+                int N = 0;
+                //подсчет людей в магазине
+                for (Buyer buyer : buyers) {
+                    if (!(buyer.getState().equals("TERMINATED"))) {
+                        ++N;
+                    }
+                }
+                if (time < 30) {
+                    if( N>=time+10 ) count=0;
+                   else count = Util.rnd(2);
+                }
+                else {
+                        if (N <= 40 + (30 - time)) count = 1;
+                        else count = 0;
+                    }
+                for (int i = 0; i < count; i++) {
+                    ++countBuyer;
+                    if (countBuyer % 4 == 0) {
+                        pensioneer = true;
+                    } else {
+                        pensioneer = true;
+                    }
+                    Buyer buyer = new Buyer(countBuyer, pensioneer);
+                    buyer.start();
+                    buyers.add(buyer);
+                }
+                Util.sleep(1000);
+            }
+        }
+    }
+}
