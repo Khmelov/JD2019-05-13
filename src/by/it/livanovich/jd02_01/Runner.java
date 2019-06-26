43422024866d1ff972e6039aa6a1659aa250ba77
tanyalivanovich <tanya1potapova@gmail.com>
2019-06-26 16:24:17 +0300

    completed 02_01

--- src/by/it/livanovich/jd02_01/Runner.java
+++ src/by/it/livanovich/jd02_01/Runner.java
@@ -4,33 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Runner {
+    private static List<Buyer> buyerList = new ArrayList<>();
+    static int c = 0;
 
-    public static void main(String[] args) {
-        List<Buyer> buyerList=new ArrayList<>();
-        int countBuyer=0;
-        int now=0;
-        for (int t = 1; t <120 ; t++) {
-            if (t<30 || (t<90 && t>60)) {
+    public static void main(String[] args) throws InterruptedException {
+        int countBuyer = 0;
+        for (int t = 1; t < 120; t++) {
+            if (t > 30 && t < 60) {
                 int count = Rnd.Rnd(2);
-                for (int i = 0; i <= count; i++) {
-                    countBuyer++; now+=i;
+                for (int i = 1; i <= count; i++) {
+                    countBuyer++;
+                    Buyer buyer = new Buyer(countBuyer);
+                    int d = coutStrem();
+                    if (buyerList.size() - d < 40 + (30 - t)) {
+                        buyer.start();
+                        buyerList.add(buyer);
+                    } else Thread.sleep(1000);
+                }
+            } else {
+                int count = Rnd.Rnd(2);
+                for (int i = 1; i <= count; i++) {
+                    countBuyer++;
                     Buyer buyer = new Buyer(countBuyer);
                     buyer.start();
                     buyerList.add(buyer);
-                }
-            }
-            else {
-                int count = Rnd.Rnd(2);
-                for (int i = 0; i <= count; i++) {
-                    countBuyer++; now+=i;
-                    Buyer buyer = new Buyer(countBuyer);
-                    if (now>40+(30-t)){
-                    buyer.start();
-                    buyerList.add(buyer);}
-                    else Thread.yield();
-                }
-            }
 
+                }
+            }
         }
+    }
+
+    public static int coutStrem() {
+        for (Buyer el : buyerList) {
+            try {
+                el.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            c++;
+        }
+        return c;
     }
 }
