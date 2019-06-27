308d75aa55a49a86727d0f6fb52d49d40eb07f07
Windows7 <guchi5550@gmail.com>
2019-06-27 21:09:16 +0300

    jd02_01C-

--- src/by/it/guchek/jd02_01/Bucket.java
+++ src/by/it/guchek/jd02_01/Bucket.java
@@ -7,7 +7,7 @@ public class Bucket {
         for (int i = 0; i < RandCount.randFrTo(1, 4); i++) {
 
             String newGood = Goods.getRandGood();
-            System.out.printf("                 %s%n", newGood);
+            System.out.printf("                 %s%n",  newGood);
 
 
         }
