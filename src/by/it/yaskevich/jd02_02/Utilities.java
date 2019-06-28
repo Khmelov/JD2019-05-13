236795e4dd42cb243d579c5517c66e0bc1d9f053
Mikhail Yaskevich <job.mikhail@gmail.com>
2019-06-28 14:55:40 +0200

    Задание jd02_02

---
+++ src/by/it/yaskevich/jd02_02/Utilities.java
@@ -0,0 +1,19 @@
+package by.it.yaskevich.jd02_02;
+
+class Utilities {
+    static int rnd(int max){
+        return rnd(0,max);
+    }
+
+    static int rnd(int start, int stop){
+        return start + (int)(Math.random() * (stop - start + 1));
+    }
+
+    static void sleep(int timeout){
+        try {
+            Thread.sleep(timeout / 100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
