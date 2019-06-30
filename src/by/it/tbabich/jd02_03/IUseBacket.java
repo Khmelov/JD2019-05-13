c5d20dbe8f3ccfb7dc5dc60d00f403232985b64d
Timur Babich <timur.babich@gmail.com>
2019-07-01 00:01:41 +0300

    added jd02_03

---
+++ src/by/it/tbabich/jd02_03/IUseBacket.java
@@ -0,0 +1,8 @@
+package by.it.tbabich.jd02_03;
+
+public interface IUseBacket {
+
+    void takeBacket(); //взял корзину
+
+    void putGoodsToBacket(Product product); //положил выбранный товар в корзину
+}
---
+++ src/by/it/tbabich/jd02_03/PensionersQueue.java
@@ -0,0 +1,26 @@
+package by.it.tbabich.jd02_03;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+class PensionersQueue {
+
+    private PensionersQueue() {
+    }
+
+    private static Deque<Buyer> instance = new LinkedList<>();
+
+    synchronized static void add(Buyer buyer) {
+        instance.addLast(buyer);
+    }
+
+    synchronized static Buyer extract(){
+        return instance.pollFirst();
+    }
+
+    synchronized static int getQueueSize(){
+        return instance.size();
+    }
+
+
+}
