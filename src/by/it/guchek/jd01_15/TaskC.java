322e09b68186752cdec2c006bedcb26c8aad33cd
Windows7 <guchi5550@gmail.com>
2019-06-30 13:07:52 +0300

    jd02_02A

--- src/by/it/guchek/jd01_15/TaskC.java
+++ src/by/it/guchek/jd01_15/TaskC.java
@@ -1,8 +1,11 @@
 package by.it.guchek.jd01_15;
 
 import java.io.File;
+import java.util.Scanner;
 
 public class TaskC {
+
+    private static File file = new File(getPath(TaskC.class));
 
     private static String getPath(Class<?> aClass) {
         String root = System.getProperty("user.dir");
@@ -14,5 +17,48 @@ public class TaskC {
         return path;
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line;
+        System.out.print(file.getAbsolutePath() + ">");
+
+        while (!(line = sc.nextLine()).equals("end")) {
+            if (line.contains("cd")) {
+
+                String[] commandLines = line.trim().split(" ");
+                if (commandLines[0].equals("cd..")) {
+                    file = new File(file.getParent());
+                    System.out.println(file.getAbsolutePath());
+                } else {
+                    StringBuilder sb = new StringBuilder(file.getAbsolutePath());
+                    sb.append('/').append(commandLines[1]);
+                    String files = sb.toString();
+                    System.out.println(files);
+                }
+            }
+            if (line.equals("dir")) {
+                File[] files = file.listFiles();
+
+                for (int i = 0; i < files.length; i++) {
+
+                        System.out.printf("<DIR>  %17s", files[i].getName());
+                        System.out.println();
+                }
+                System.out.println(file.getAbsolutePath() + ">");
+            }
+            //else System.out.println("Правильно вводите комманды");
+        }
+
+    }
 
 }
+
+
+
+
+
+
+
+
+    //SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+    //date.format(files[i].lastModified())
