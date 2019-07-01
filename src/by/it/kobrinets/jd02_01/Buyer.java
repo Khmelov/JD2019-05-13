15874cfa83177ada2b1a3087367a1f12beedb0bb
y.kobrinets@gmail.com <la$t_legend97>
2019-07-01 12:44:26 +0300

    jd01_15-jd02-02

---
+++ src/by/it/kobrinets/jd02_01/Buyer.java
@@ -0,0 +1,55 @@
+package by.it.kobrinets.jd02_01;
+
+public class Buyer extends Thread implements IBuyer, IUseBasket {
+
+    public Buyer(int number) {
+        super("Buyer №" + number);
+    }
+
+    @Override
+    public void run() {
+
+        enterToMarket();
+        takeBasket();
+        chooseGoods();
+        putGoodsToBasket();
+        goOut();
+    }
+
+    @Override
+    public void enterToMarket() {
+        System.out.println(this + " enter to the market");
+    }
+
+    @Override
+    public void chooseGoods() {
+        System.out.println(this + " start choosing goods");
+        int timeout = Util.rnd(1000, 2000);
+        Util.sleep(timeout);
+    }
+
+    @Override
+    public void goOut() {
+        System.out.println(this + " left the market");
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
+    @Override
+    public void takeBasket() {
+        System.out.println(this + " took the basket");
+    }
+
+    @Override
+    public void putGoodsToBasket() {
+        int count;
+        for (count = 1; count < 5; count++) {
+            String good = Goods.getRandomGood();
+            System.out.println(this + " chose " + count + " " + good + " put in the basket");
+            System.out.println(this + " stop choosing goods");
+        }
+    }
+}
