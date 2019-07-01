2780a19b51658699599924f33392b0733d0723d7
AdamvichAS <anton.adamovich1010@gmail.com>
2019-07-01 14:58:59 +0300

    jd02_03_complete

---
+++ src/by/it/adamovichjr/jd02_03/IBuer.java
@@ -0,0 +1,8 @@
+package by.it.adamovichjr.jd02_03;
+
+public interface IBuer {
+    void enterToMarket();
+    String choseGoods();
+    void goToQueue() throws InterruptedException;
+    void goToOut();
+}
