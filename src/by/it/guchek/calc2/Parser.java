7e35016343cc9ecff7096da496ad0e4c353d2ff6
Windows7 <guchi5550@gmail.com>
2019-07-04 22:49:19 +0300

    jd03_03calc2

--- src/by/it/guchek/calc2/Parser.java
+++ src/by/it/guchek/calc2/Parser.java
@@ -91,35 +91,4 @@ class Parser {
    }
 
 
-   /*Var calc(String expression) throws CalcException {
-
-      //2.0*2.0
-      expression=expression.replaceAll("\\s", "");
-      String [] operand = expression.split(Patterns.OPERATION);
-      //A=9
-      Var two = Var.createVar(operand[1]);
-      if(expression.contains("=")){
-         return Var.saveVar(operand[0], two);
-      }
-      Var one = Var.createVar(operand[0]);
-      if ( one==null || two==null )  //TODO create error
-      {
-         throw new CalcException("Пустая переменная");
-      }
-      Pattern p = Pattern.compile(Patterns.OPERATION);
-      Matcher m = p.matcher(expression);      //выщимливаем знаки
-      if (m.find()){
-         String operation = m.group();
-         switch (operation){
-            case "+": return one.add(two);
-            case "-": return one.sub(two);
-            case "*": return one.mul(two);
-            case "/": return one.div(two);
-            //case "=": return Var.saveVar(operand[0], two);
-         }
-      }
-
-      throw new CalcException("Ошибка парсинга выражений");
-
-   }*/
 }
