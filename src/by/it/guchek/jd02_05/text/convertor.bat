eb6aba652d311c2d94ec2358b4a70c5ff0d604f3
Windows7 <guchi5550@gmail.com>
2019-07-05 04:00:43 +0300

    jd02_05

--- src/by/it/guchek/jd02_05/text/convertor.bat
+++ src/by/it/guchek/jd02_05/text/convertor.bat
@@ -2,3 +2,4 @@ set path=D:\JDK\java-se-8u40-ri\bin
 native2ascii -encoding utf-8 message.txt ../resources/message.properties
 native2ascii -encoding utf-8 message_ru_RU.txt ../resources/message_ru_RU.properties
 native2ascii -encoding utf-8 message_be_BY.txt ../resources/message_be_BY.properties
+native2ascii -encoding utf-8 message_us_EN.txt ../resources/message_us_EN.properties
