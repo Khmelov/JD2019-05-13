282be03fad9e2d578cee677af3817181298ccaf5
tanyalivanovich <tanya1potapova@gmail.com>
2019-07-05 12:15:31 +0300

    completed 02_05

--- src/by/it/livanovich/jd02_02/Cashier.java
+++ src/by/it/livanovich/jd02_02/Cashier.java
@@ -1,37 +1,38 @@
 package by.it.livanovich.jd02_02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cashier implements Runnable {
     static final Object monitor = new Object();
     private String name;
+    private List<Cashier> cashiers=new ArrayList<>();
 
     Cashier(int number) {
         name = "Кассир №" + number + " ";
     }
 
     @Override
-    public String toString() {
-        return name;
-    }
-
-    @Override
     public void run() {
         while (!Dispatcher.planIsDone()) {
-            Buyer buyer = Queue.deductBuyer();
-            if (buyer != null) {
-                System.out.println(this + "начал обслуживать " + buyer);
-                int timeout = Rnd.Rnd(2000, 5000);
-                Rnd.sleep(timeout);
-                System.out.println("В корзине " + buyer + ": ");
-                double sum = 0;
-                Good good;
-                while ((good = buyer.getBacket().extractBasket()) != null) {
-                    System.out.println(good);
-                    sum += good.getPrice();
-                }
-                System.out.printf("Итого: %4.2f\n", sum);
-                System.out.println(this + "закончил обслуживать " + buyer);
-                synchronized (buyer) {
-                    buyer.notifyAll();
+            if (countWorkCashier(needsOfCashiers())) {
+                Buyer buyer = Queue.deductBuyer();
+                if (buyer != null) {
+                    System.out.println(this + "начал обслуживать " + buyer);
+                    int timeout = Rnd.Rnd(2000, 5000);
+                    Rnd.sleep(timeout);
+                    System.out.println("В корзине " + buyer + ": ");
+                    double sum = 0;
+                    Good good;
+                    while ((good = buyer.getBacket().extractBasket()) != null) {
+                        System.out.println(good);
+                        sum += good.getPrice();
+                    }
+                    System.out.printf("Итого: %4.2f\n", sum);
+                    System.out.println(this + "закончил обслуживать " + buyer);
+                    synchronized (buyer) {
+                        buyer.notifyAll();
+                    }
                 }
             } else synchronized (monitor) {
                 try {
@@ -41,5 +42,30 @@ public class Cashier implements Runnable {
                 }
             }
         }
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    static int needsOfCashiers (){
+        int needs= (int) Math.ceil(Queue.getQueueOfBuyersSize()/5);
+        return needs;
+    }
+
+    boolean countWorkCashier (int needs){
+        if (cashiers.size()<needs){
+            cashiers.add(this);
+        }
+        else if (cashiers.size()<needs){
+            cashiers.remove(this);
+            return false;
+        }
+        else {
+            if (!cashiers.contains(this));
+            return false;
+        }
+        return true;
     }
 }
---
+++ src/by/it/livanovich/jd02_05/text/conventor.bat
@@ -0,0 +1,3 @@
+native2ascii -encoding UTF-8 messages.txt ..\resources\messages.properties
+native2ascii -encoding UTF-8 messages_ru_RU.txt ..\resources\messages_ru_RU.properties
+native2ascii -encoding UTF-8 messages_be_BY.txt ..\resources\messages_be_BY.properties
