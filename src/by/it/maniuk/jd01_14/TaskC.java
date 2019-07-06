e7188beb304432396e0eee301d2f37e5c59681b7
DiMan <dfManiuk@gmail.com>
2019-07-06 20:03:06 +0300

    Merge branch 'master' of https://github.com/Khmelov/JD2019-05-13
    # Conflicts:
    #	src/by/it/_examples_/jd01_14/Utils.java

--- src/by/it/maniuk/jd01_14/TaskC.java
+++ src/by/it/maniuk/jd01_14/TaskC.java
@@ -80,54 +80,6 @@ public class TaskC {
 }
 
 
-//public class TaskC {
-//
-//    private static String filePath = getPath();
-//    private static String fileOutPath = getFilePath();
-//
-//    private static String getFilePath() {
-//        return getPath() + "resultTaskC.txt";
-//    }
-//
-//    private static String getPath() {
-//        String root = System.getProperty("user.dir");
-//        String name = TaskB.class.getName();
-//        String simpleName = TaskB.class.getSimpleName();
-//        String separator = File.separator;
-//        String path = name.replace(simpleName, "").replace(".", separator);
-//        path = root + separator + "src" + separator + path;
-//        return path;
-//    }
-//
-//    private static void tree(File file, PrintWriter out) {
-//
-//        File[] files = file.listFiles();
-//        assert files != null;
-//        for (File fileRek : files) {
-//            if (fileRek.isFile()) {
-//                out.println("file:" + fileRek.getName());
-//                System.out.println("file:" + fileRek.getName());
-//            } else if (fileRek.isDirectory()) {
-//                out.println("dir:" + fileRek.getName());
-//                System.out.println("dir:" + fileRek.getName());
-//                tree(fileRek, out);
-//            }
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//
-//        try (PrintWriter out = new PrintWriter(
-//                new FileWriter(fileOutPath))) {
-//            File file = new File(filePath).getParentFile();
-//            tree(file, out);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//}
 
 
 
