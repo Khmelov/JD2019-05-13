2d94f7b45b664053ae6267e8a789a9d61c683668
RudnitskTimofei <3761544Radiohead>
2019-07-08 02:15:10 +0300

    jd02_05

---
+++ src/by/it/trudnitski/jd02_05/names/Messages.java
@@ -0,0 +1,10 @@
+package by.it.trudnitski.jd02_05.names;
+
+public interface Messages {
+
+    String WELCOME="message.welcome";
+    String QUESTION="message.question";
+    String LANGUAGE="message.language";
+
+
+}
